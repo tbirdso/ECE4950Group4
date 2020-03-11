90e669d026736f4c4ecd0c51948b05aff5028434
tbirdso <40648863+tbirdso@users.noreply.github.com>
2020-03-11 17:16:12 -0400

    Updated rect to polar

--- Project3/rect_to_polar_v2.m
+++ Project3/rect_to_polar_v2.m
@@ -1,4 +1,17 @@
-function Angles = rect_to_polar_v2( center_coords, numObjects )
+
+% PURPOSE - Convert rectangular object coordinates to angle for
+%           motor position
+% INPUTS
+%   - X, Y coordinates == center coords of each object
+% OUTPUTS
+%   - Vector of angles (radians) ordered ascending
+% NOTES
+%   - Theta == 0 is at the bottom of the map
+%   - Positive angles are in RH plane
+%   - Negative angles are in LH plane
+%   - Camera origin (0,0) is at NW corner
+%   - Game board origin is at image center
+function Angles_sorted = rect_to_polar_v2( center_coords )
 
 % Update these to actual frame dimensions
 IMG_LENGTH = 826;
@@ -9,6 +22,8 @@ IMG_HEIGHT = 593;
 % so we can calculate relative angles
 MotX = IMG_LENGTH/2;
 MotY = IMG_HEIGHT/2;
+
+numObjects = size(center_coords, 1);
 Angles = zeros(numObjects, 1);
 
 
@@ -16,20 +31,21 @@ for i = 1:numObjects
     Xdiff = center_coords(i,1) - MotX;
     Ydiff = center_coords(i,2) - MotY;
     Angles(i) = atan(Ydiff/Xdiff);
-    Angles(i) = Angles(i) * 180/3.1415926535;
     
     % Quandrant dependent angle math
     if ((Xdiff > 0) && (Ydiff < 0))
         Angles(i) = Angles(i) * -1;
     elseif((Xdiff < 0) && (Ydiff < 0))
-        Angles(i) = 180 - Angles(i);
+        Angles(i) = pi - Angles(i);
     elseif((Xdiff < 0) && (Ydiff > 0))
-        Angles(i) = 180 + (-1 * Angles(i));
+        Angles(i) = pi + (-1 * Angles(i));
     else
         % may need to fix this, this is 4th quadrant
         Angles(i) = Angles(i);
     end
 end
+
+Angles_sorted = sort(Angles);
 
 
 end
